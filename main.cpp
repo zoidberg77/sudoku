8cc5116dbae2c6834965b0729a89ab255fe7f56d
Kevin Sidak <zoidberg77@users.noreply.github.com>
2017-04-03 21:20:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,17 +20,6 @@ int main()
     std::cout << "It took me " << time_span.count() << " seconds.";
     std::cout << std::endl;
 
-    Checker y;
-    steady_clock::time_point t3 = steady_clock::now();
-    y.check();
-
-    steady_clock::time_point t4 = steady_clock::now();
-
-    duration<double> time_span2 = duration_cast<duration<double>>(t4 - t3);
-
-    std::cout << "It took me " << time_span2.count() << " seconds.";
-    std::cout << std::endl;
-
     
     return 0;
 }
